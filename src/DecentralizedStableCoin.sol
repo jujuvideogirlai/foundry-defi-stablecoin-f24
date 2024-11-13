11c30493b493f7af0755ec4fab8a71f18fde8933
juliapolbach <julia@alternaenergetica.com>
2024-11-13 12:47:23 +0100

    🎉 initial commit

--- src/DecentralizedStableCoin.sol
+++ src/DecentralizedStableCoin.sol
@@ -3,6 +3,7 @@ pragma solidity ^0.8.18;
 
 import {ERC20, ERC20Burnable} from "@openzeppelin/contracts/token/ERC20/extensions/ERC20Burnable.sol";
 import {Ownable} from "@openzeppelin/contracts/access/Ownable.sol";
+import {console} from "forge-std/Test.sol";
 
 /**
  * @title   DecentralizedStableCoin
@@ -13,20 +14,23 @@ import {Ownable} from "@openzeppelin/contracts/access/Ownable.sol";
  *
  * This is the contract meant to be governed by DSCEngine. This contract is just the ERC20 implementation of our stablecoin system.
  */
-
 contract DecentralizedStableCoin is ERC20Burnable, Ownable {
     //errors
-    error DecentralizedStableCoin__InsufficientAmount();
+    error DecentralizedStableCoin__AmountMustBeMoreThanZero();
     error DecentralizedStableCoin__BurnAmountExceedsBalance();
     error DecentralizedStableCoin__NotZeroAddress();
 
-    constructor() ERC20("DecentralizedStableCoin", "DSC") {}
+    address public immutable i_owner;
+
+    constructor() ERC20("DecentralizedStableCoin", "DSC") {
+        i_owner = msg.sender;
+    }
 
     // onlyOwner modifier is used to restrict the access to the function to the owner of the contract.
     function burn(uint256 _amount) public override onlyOwner {
         uint256 balance = balanceOf(msg.sender);
         if (_amount <= 0) {
-            revert DecentralizedStableCoin__InsufficientAmount();
+            revert DecentralizedStableCoin__AmountMustBeMoreThanZero();
         }
         if (balance < _amount) {
             revert DecentralizedStableCoin__BurnAmountExceedsBalance();
@@ -35,17 +39,18 @@ contract DecentralizedStableCoin is ERC20Burnable, Ownable {
         super.burn(_amount);
     }
 
-    function mint(
-        address _to,
-        uint256 _amount
-    ) external onlyOwner returns (bool) {
+    function mint(address _to, uint256 _amount) external onlyOwner returns (bool) {
         if (_to == address(0)) {
             revert DecentralizedStableCoin__NotZeroAddress();
         }
         if (_amount <= 0) {
-            revert DecentralizedStableCoin__InsufficientAmount();
+            revert DecentralizedStableCoin__AmountMustBeMoreThanZero();
         }
         _mint(_to, _amount);
         return true;
+    }
+
+    function getOwner() public view returns (address) {
+        return i_owner;
     }
 }
